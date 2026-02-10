66573593214761178ba6a39b8ffaf7e1c8b31889
Mehdi <mehdi.el.mestari.pro@gmail.com>
2026-02-10 10:47:53 +0100

    End Of Section 8

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material"
+import { Grid2 } from "@mui/material"
 import type { Product } from "../../app/models/product"
 import ProductCard from "./ProductCard"
 
@@ -6,14 +6,15 @@ type Props = {
     products: Product[]
 }
 
-export default function ProductList({products}: Props) {
-  return (
-    <>
-        <Box sx={{display: 'flex', flexWrap: 'wrap', gap: 3, justifyContent: 'center'}}>
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product}/>
-        ))}
-        </Box>
-    </>
-  )
+export default function ProductList({ products }: Props) {
+    return (
+        <Grid2 container spacing={3}>
+            {products.map(product => (
+                <Grid2 size={3} display='flex' key={product.id}>
+                    <ProductCard product={product} />
+                </Grid2>
+
+            ))}
+        </Grid2>
+    )
 }
